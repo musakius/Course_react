2ca2ba15c0b83b45b3e5e65e7f82aefc2019c011
Petr <musakius.work@gmail.com>
2020-04-12 14:42:38 +0300

    homework5.2

--- src/components/error-component/error-component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./error-component.css";
-
-const ErrorComponent = () => {
-  return <div className="error-component jumbotron">Error</div>;
-};
-
-export default ErrorComponent;
